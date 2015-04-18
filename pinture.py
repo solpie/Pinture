11fac14edebf1e5fe055bf593d3e4f52d3f36fe7
solpie <solpie.net@gmail.com>
2015-04-18 19:08:20 +0800

    normal path

--- pinture.py
+++ pinture.py
@@ -9,7 +9,7 @@ from random import randint
 
 class Pinture():
     def __init__(self):
-        self.imagePath = os.path.normpath('dl\\')
+        self.imagePath = os.path.normpath('dl/')
         self.images = []
         self.updateImage()
         pass
@@ -18,10 +18,10 @@ class Pinture():
         self.images = []
         for root, dirs, files in os.walk(self.imagePath):
             for filespath in files:
-                filename = os.path.join(root, filespath)
+                filename = os.path.join(root, filespath).replace('\\', '/')
                 img = Img()
                 img.path = filename
-                img.refPath = filename.replace(self.imagePath, '').replace('\\', '/')
+                img.refPath = filename.replace(self.imagePath, '')
                 img.createMeta(filename)
                 self.images.append(img)
                 print('[load img]: ', filename)
